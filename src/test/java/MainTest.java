c71e1ffb1d13d12a15a52c6d6b629ad1fffaa1e1
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-03-19 00:49:59 +0300

    last commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -27,18 +29,20 @@ public class MainTest {
     @BeforeEach
     void setUp() {
         task1 = new Task("Java Collections", "Write List Interface",
-                "Ann", Status.IN_QUEUE, Priority.LOW);
+                        "Ann", Priority.LOW, Status.IN_QUEUE);
         task2 = new Task("Java Collections", "Write Set Interface",
-                "Ann", Status.ASSIGNED, Priority.MED);
+                        "Ann", Priority.MED, Status.ASSIGNED);
         task3 = new Task("Java Collections", "Write Map Interface",
-                "Bob", Status.IN_QUEUE, Priority.HIGH);
+                        "Bob", Priority.HIGH, Status.IN_QUEUE);
 
         taskSet1 = new HashSet<>();
         taskSet1.add(task1);
+
         taskSet2 = new HashSet<>();
-        taskSet1.add(task2);
+        taskSet2.add(task2);
+
         taskSet3 = new HashSet<>();
-        taskSet1.add(task3);
+        taskSet3.add(task3);
 
         taskData = new TaskData(taskSet1, taskSet2, taskSet3, new HashSet<>());
     }
@@ -46,107 +50,49 @@ public class MainTest {
     @DisplayName("Task sınıfı doğru access modifiers sahip mi")
     @Test
     public void testTaskAccessModifiers() throws NoSuchFieldException {
-        Field assigneeFields = task1.getClass().getDeclaredField("assignee");
-        Field descriptionsFields = task1.getClass().getDeclaredField("description");
-        Field projectFields = task1.getClass().getDeclaredField("project");
-        Field priorityFields = task1.getClass().getDeclaredField("priority");
-        Field statusFields = task1.getClass().getDeclaredField("status");
-
-        assertEquals(assigneeFields.getModifiers(), 2);
-        assertEquals(descriptionsFields.getModifiers(), 2);
-        assertEquals(projectFields.getModifiers(), 2);
-        assertEquals(priorityFields.getModifiers(), 2);
-        assertEquals(statusFields.getModifiers(), 2);
-    }
-
-    @DisplayName("Task sınıfı doğru typelara sahip mi")
-    @Test
-    public void testTaskTypes() throws NoSuchFieldException {
-        assertThat(task1.getAssignee(), instanceOf(String.class));
-        assertThat(task1.getDescription(), instanceOf(String.class));
-        assertThat(task1.getPriority(), instanceOf(Priority.class));
-        assertThat(task1.getProject(), instanceOf(String.class));
-        assertThat(task1.getStatus(), instanceOf(Status.class));
-    }
-
-    @DisplayName("TaskData sınıfı doğru access modifiers sahip mi")
-    @Test
-    public void testTaskDataAccessModifiers() throws NoSuchFieldException {
-        Field annTasksField = taskData.getClass().getDeclaredField("annsTasks");
-        Field bobTasksField = taskData.getClass().getDeclaredField("bobsTasks");
-        Field carolTasksField = taskData.getClass().getDeclaredField("carolsTasks");
-        Field unassignedTasksField = taskData.getClass().getDeclaredField("unassignedTasks");
-
-        assertEquals(annTasksField.getModifiers(), 2);
-        assertEquals(bobTasksField.getModifiers(), 2);
-        assertEquals(carolTasksField.getModifiers(), 2);
-        assertEquals(unassignedTasksField.getModifiers(), 2);
-    }
-
-    @DisplayName("TaskData getTasks method doğru çalışıyor mu ?")
-    @Test
-    public void testGetTasksMethod() {
-        assertEquals(taskData.getTasks("ann"), taskSet1);
-        assertEquals(taskData.getTasks("bob"), taskSet2);
-        assertEquals(taskData.getTasks("carol"), taskSet3);
+        assertEquals(Modifier.PRIVATE, task1.getClass().getDeclaredField("assignee").getModifiers());
+        assertEquals(Modifier.PRIVATE, task1.getClass().getDeclaredField("description").getModifiers());
+        assertEquals(Modifier.PRIVATE, task1.getClass().getDeclaredField("project").getModifiers());
+        assertEquals(Modifier.PRIVATE, task1.getClass().getDeclaredField("priority").getModifiers());
+        assertEquals(Modifier.PRIVATE, task1.getClass().getDeclaredField("status").getModifiers());
     }
 
     @DisplayName("TaskData getUnion method doğru çalışıyor mu ?")
     @Test
     public void testGetUnionMethod() {
-        Set<Task> taskSet = new HashSet<>();
-        taskSet.add(task1);
-        Set<Task> taskSet2 = new HashSet<>();
-        taskSet.add(task2);
-
-        Set<Task> totals = taskData.getUnion(taskSet, taskSet2);
-        assertEquals(totals.size(), 2);
+        Set<Task> totals = taskData.getUnion(taskSet1, taskSet2);
+        assertEquals(2, totals.size());
     }
 
-    @DisplayName("TaskData getIntersect() method doğru çalışıyor mu ?")
+    @DisplayName("TaskData getIntersection() method doğru çalışıyor mu ?")
     @Test
-    public void testGetIntersectMethod() throws NoSuchFieldException {
-        Set<Task> taskSet = new HashSet<>();
-        taskSet.add(task1);
-        taskSet.add(task2);
-        Set<Task> taskSet2 = new HashSet<>();
-        taskSet2.add(task2);
+    public void testGetIntersectMethod() {
+        Set<Task> taskSet = new HashSet<>(Set.of(task1, task2));
+        Set<Task> taskSet2 = new HashSet<>(Set.of(task2));
 
         Set<Task> intersections = taskData.getIntersection(taskSet, taskSet2);
-
-        for(Task task: intersections){
-            assertEquals(task, task2);
-        }
-
-        assertEquals(intersections.size(), 1);
+        assertEquals(1, intersections.size());
+        assertEquals(task2, intersections.iterator().next());
     }
 
     @DisplayName("TaskData getDifference() method doğru çalışıyor mu ?")
     @Test
-    public void testGetDifferenceMethod() throws NoSuchFieldException {
-        Set<Task> taskSet = new HashSet<>();
-        taskSet.add(task1);
-        taskSet.add(task2);
-        Set<Task> taskSet2 = new HashSet<>();
-        taskSet2.add(task2);
+    public void testGetDifferenceMethod() {
+        Set<Task> taskSet = new HashSet<>(Set.of(task1, task2));
+        Set<Task> taskSet2 = new HashSet<>(Set.of(task2));
 
-        Set<Task> differences = taskData.getDifferences(taskSet, taskSet2);
-
-        for(Task task: differences){
-            assertEquals(task, task1);
-        }
-
-        assertEquals(differences.size(), 1);
+        Set<Task> differences = taskData.getDifference(taskSet, taskSet2);
+        assertEquals(1, differences.size());
+        assertEquals(task1, differences.iterator().next());
     }
 
     @DisplayName("findUniqueWords doğru çalışıyor mu ?")
     @Test
     public void testFindUniqueWordsMethod() {
-        assertEquals(StringSet.findUniqueWords().size(), 143);
+        assertEquals(143, StringSet.findUniqueWords().size());
 
         List<String> results = StringSet.findUniqueWords().stream().collect(Collectors.toList());
-        assertEquals(results.get(0), "a");
-        assertEquals(results.get(results.size()-1), "wrote");
-
+        assertEquals("a", results.get(0));
+        assertEquals("wrote", results.get(results.size() - 1));
     }
 }
